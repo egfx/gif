7f4d669163fc82f28b39de4eb00cf8be39e8c5b2
Ilya Berelson <segrafix@gmail.com>
2020-05-03 22:27:16 -0700

    Update anigif.js

--- anigif/anigif.js
+++ anigif/anigif.js
@@ -99,7 +99,7 @@
                         })
            }
            else
-            domtoimage.toBlob(self.el, {bgcolor: '#ffffff00', style: {background-color: 'transparent'}})
+            domtoimage.toBlob(self.el, {style: {background-color: '#ffffff00'}})
                 .then(function (blobFrame) {
                     
                     if(!document.getElementById('someCanvasId')){
@@ -235,7 +235,7 @@
             document.body.appendChild(this.frames[i]);
             this.replaceSvgWithCanvas(this.frames[i]);
         
-	    domtoimage.toBlob(self.frames[i], {bgcolor: '#ffffff00', style: {background-color: 'transparent'}})
+	    domtoimage.toBlob(self.frames[i], {style: {background-color: '#ffffff00'}})
                 .then(function (blobFrame) {
                     
                     if(!document.getElementById('otherCanvasId')){
