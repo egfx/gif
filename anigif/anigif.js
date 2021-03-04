a3539c3f9999cca978a37983c912245603046995
Ilya Berelson <segrafix@gmail.com>
2021-03-04 13:50:50 -0800

    Update anigif.js

--- anigif/anigif.js
+++ anigif/anigif.js
@@ -99,12 +99,10 @@
                         })
            }
            else
-            window.html2canvas([ self.el ], {
-                    onrendered: function(canvas) {
-                        self.resizeImage(canvas, self.options.ratio, function(err, canvas_small) {
-                            cba(null, canvas_small);    
-                        })
-                    }
+            window.html2canvas([ self.el ]).then(function(canvas) {
+		self.resizeImage(canvas, self.options.ratio, function(err, canvas_small) {
+		    cba(null, canvas_small);    
+		})
             });
            },
         
@@ -210,12 +208,10 @@
             document.body.appendChild(this.frames[i]);
             this.replaceSvgWithCanvas(this.frames[i]);
 	    
-   	    window.html2canvas([ self.frames[i] ], {
-                onrendered: function(canvas) {
-                    handleImage(canvas);
-                    self.frames[i].parentElement.removeChild(self.frames[i]);
-                }
-            }); 
+   	    window.html2canvas([ self.frames[i] ]).then(function(canvas){
+                handleImage(canvas);
+            	self.frames[i].parentElement.removeChild(self.frames[i]);
+            });
           }
         },
         
