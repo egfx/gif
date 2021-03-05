bc31a5eb85639246701fcc969a99403a2c2a4e4e
Ilya Berelson <segrafix@gmail.com>
2021-03-05 12:16:38 -0800

    Update anigif.js

--- anigif/anigif.js
+++ anigif/anigif.js
@@ -275,7 +275,7 @@
         composeAnimatedGif: function(cba) {
             var self = this
             //console.log("starting gif composition")
-            var encoder = new window.GIFEncoder_WebWorker({base_url: "https://cdn.jsdelivr.net/gh/egfx/anirecord/jsgif@master/"});
+            var encoder = new window.GIFEncoder_WebWorker({base_url: "https://cdn.jsdelivr.net/gh/egfx/anirecord@master/anigif/jsgif/"});
             //encoder.setTransparent();
 	    encoder.setRepeat(0); //auto-loop
             encoder.setDelay(1000/this.options.framesPerSecond);
