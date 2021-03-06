31ec8bcc248286c6f7e7486a90061cb4ca88dda1
Ilya Berelson <segrafix@gmail.com>
2021-03-06 09:06:37 -0800

    Update anigif.js

--- anigif/anigif.js
+++ anigif/anigif.js
@@ -281,7 +281,7 @@
             encoder.setThreads(this.options.cores);
 	    encoder.setTransparent(0x00FF00);
             encoder.start();
-             for (let i=0; i<this.images.length; i++) {
+             for (var i=0; i<this.images.length; i++) {
                 var context = this.images[i].getContext('2d');
                 encoder.addFrame(context);
             }
